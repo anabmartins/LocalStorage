8ae3a3880051cd158fdbd6122f23e8b5bc6b7465
Ana Beatriz Martins <99266498+anabmartins@users.noreply.github.com>
2022-05-31 13:00:02 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,24 +3,34 @@ const addUsuario = () => {
     nome = document.getElementById('nome').value
     senha = document.getElementById('senha').value
     email = document.getElementById('email').value
-    
-    if(localStorage.getItem('usuarios')!= null){
-        usuarios = JSON.parse(localStorage.getItem('usuarios'))
+    erro = document.querySelector('.erro')
+
+    if (nome == "" || email == '' || senha == "") {
+        erro.style.display = 'block'
+    } else {
+        erro.style.display = 'none'
+        window.alert('Registrado com sucesso!')
     }
-    
+
+    if (localStorage.getItem('usuarios') != null) {
+        usuarios = JSON.parse(localStorage.getItem('usuarios'))
+        nome = document.getElementById('nome').value = ''
+        senha = document.getElementById('senha').value = ''
+        email = document.getElementById('email').value = ''
+    }
+
     usuarios.push([nome, email, senha])
     localStorage.setItem('usuarios', JSON.stringify(usuarios))
 }
 
 const listUsuarios = () => {
-let tbody = document.getElementById('table-linha')
-if(localStorage.getItem('usuarios')!= null){
-    usuarios = JSON.parse(localStorage.getItem('usuarios'))
-    usuarios.forEach(usuario => {
-        tbody.innerHTML += "<tr> <td>"+usuario[0]+"</td> <td>"+usuario[1]+"</td></tr>"
-    });
-}else {
-    tbody.innerHTML = "<tr> <td>Vazio</td> <td>Vazio</td> </tr>"     
+    let tbody = document.getElementById('table-linha')
+    if (localStorage.getItem('usuarios') != null) {
+        usuarios = JSON.parse(localStorage.getItem('usuarios'))
+        usuarios.forEach(usuario => {
+            tbody.innerHTML += "<tr> <td>" + usuario[0] + "</td> <td>" + usuario[1] + "</td></tr>"
+        });
+    } else {
+        tbody.innerHTML = "<tr> <td>Vazio</td> <td>Vazio</td> </tr>"
+    }
 }
-}
-
