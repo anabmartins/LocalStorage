afba9f06e38f46a9522e61da05963c25d4ccf23d
Ana Beatriz Martins <99266498+anabmartins@users.noreply.github.com>
2022-05-31 12:18:41 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,8 +3,24 @@ const addUsuario = () => {
     nome = document.getElementById('nome').value
     senha = document.getElementById('senha').value
     email = document.getElementById('email').value
-
+    
+    if(localStorage.getItem('usuarios')!= null){
+        usuarios = JSON.parse(localStorage.getItem('usuarios'))
+    }
+    
     usuarios.push([nome, email, senha])
-    console.log('users', usuarios);
+    localStorage.setItem('usuarios', JSON.stringify(usuarios))
+}
+
+const listUsuarios = () => {
+let tbody = document.getElementById('table-linha')
+if(localStorage.getItem('usuarios')!= null){
+    usuarios = JSON.parse(localStorage.getItem('usuarios'))
+    usuarios.forEach(usuario => {
+        tbody.innerHTML += "<tr> <td>"+usuario[0]+"</td> <td>"+usuario[1]+"</td></tr>"
+    });
+}else {
+    tbody.innerHTML = "<tr> <td>Vazio</td> <td>Vazio</td> </tr>"     
+}
 }
 
