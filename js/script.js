031ca2ca1d5fc88835548d6f251fd0aecd6cf0a4
Ana Beatriz Martins <99266498+anabmartins@users.noreply.github.com>
2022-06-07 13:02:10 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,8 +1,52 @@
-nome = document.getElementById('nome')
-senha = document.getElementById('senha')
-email = document.getElementById('email')
 let usuarios = []
 
+// adicionar ao localstorage
+function addUsuario() {
+    nome = document.getElementById('nome').value
+    senha = document.getElementById('senha').value
+    email = document.getElementById('email').value
+    csenha = document.getElementById('confsenha').value
+
+    if (nome == '' || email == '' || senha == '') {
+        document.getElementById('alerta').innerHTML = "<font color='red'>Preencha o formulário corretamente! </font>"
+
+    } else {
+
+        document.getElementById('alerta').innerHTML = "<font color='green'>Registrado com sucesso! </font>"
+
+        if (localStorage.getItem('usuarios') != null) {
+            usuarios = JSON.parse(localStorage.getItem('usuarios'))
+        }
+        document.getElementById('nome').value = ''
+        document.getElementById('senha').value = ''
+        document.getElementById('email').value = ''
+
+        usuarios.push([nome, email, senha])
+        localStorage.setItem('usuarios', JSON.stringify(usuarios))
+    }
+}
+
+function listUsuarios() {
+    let tbody = document.getElementById('table-linha')
+    if (localStorage.getItem('usuarios') != null) {
+        usuarios = JSON.parse(localStorage.getItem('usuarios'))
+        usuarios.forEach((usuario, index) => {
+            tbody.innerHTML += "<tr> <td>" + usuario[0] + "</td> <td>" + usuario[1] +
+                "</td> " + "<td><button id='trash' onclick='deleteUsuario(" + index + ")'> \u{1F5D1} </button></td></tr>"
+        });
+    } else {
+        tbody.innerHTML = "<tr> <td>Vazio</td> <td>Vazio</td> </tr>"
+    }
+}
+
+const deleteUsuario = (index) => {
+    usuarios = JSON.parse(localStorage.getItem('usuarios'))
+    let auxUsuarios = usuarios.filter((v, i) => i != index)
+    localStorage.setItem('usuarios', JSON.stringify(auxUsuarios))
+    document.location.reload(true)
+}
+
+// verificar email
 function validarEmail(field) {
     email = field.value.substring(0, field.value.indexOf('@'))
     dominio = field.value.substring(field.value.indexOf('@') + 1, field.value.length)
@@ -23,35 +67,15 @@ function validarEmail(field) {
     }
 }
 
-function addUsuario() {
-    nome = document.getElementById('nome').value
+// validar senha
+function validarSenha() {
     senha = document.getElementById('senha').value
-    email = document.getElementById('email').value
-
-    if (nome == '' || email == '' || senha == '') {
-        document.getElementById('alerta').innerHTML = "<font color='red'>Preencha o formulário corretamente! </font>"
+    csenha = document.getElementById('confsenha').value
+    
+    if (senha.value == csenha.value) {
+        csenha.innerHTML = "<font color='green'>Senha correta</font>"
     } else {
-        document.getElementById('alerta').innerHTML = "<font color='green'>Registrado com sucesso! </font>"
+        csenha.innerHTML = "<font color='red'>Senha incorreta</font>"
     }
-
-    if (localStorage.getItem('usuarios') != null) {
-        usuarios = JSON.parse(localStorage.getItem('usuarios'))
-        nome = document.getElementById('nome').value = ''
-        senha = document.getElementById('senha').value = ''
-        email = document.getElementById('email').value = ''
-    }
-    usuarios.push([nome, email, senha])
-    localStorage.setItem('usuarios', JSON.stringify(usuarios))
 }
 
-function listUsuarios(){
-    let tbody = document.getElementById('table-linha')
-    if (localStorage.getItem('usuarios') != null) {
-        usuarios = JSON.parse(localStorage.getItem('usuarios'))
-        usuarios.forEach(usuario => {
-            tbody.innerHTML += "<tr> <td>" + usuario[0] + "</td> <td>" + usuario[1] + "</td></tr>"
-        });
-    } else {
-        tbody.innerHTML = "<tr> <td>Vazio</td> <td>Vazio</td> </tr>"
-    }
-}
